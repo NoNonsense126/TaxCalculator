92567fb2816652023f48390615f73872127b3c76
NoNonsense126 <retail_nns@hotmail.com>
2016-01-02 18:22:27 +0800

    Initial Commit

---
+++ TaxCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaxCalculator
+//
+//  Created by Wong You jing on 02/01/2016.
+//  Copyright © 2016 NoNonsense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TaxCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaxCalculator
+//
+//  Created by Wong You jing on 02/01/2016.
+//  Copyright © 2016 NoNonsense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
